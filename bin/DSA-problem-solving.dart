ff14387c88a38b029d5c6623ca95666717cee966
jmawya <jmawya07@gmail.com>
2026-08-27 10:59:14 +0600

    Add positive negative zero problem

---
+++ bin/DSA-problem-solving.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(){
+  // Given an integer, determine whether it is positive, negative, or zero.
+  int num=-9;
+  if(num>0){
+    print('positive number: $num');
+  }
+  else if(num<0){
+    print('Negative number:$num') ;
+
+  }
+  else{
+    print('Zero');
+  }
+
+}
