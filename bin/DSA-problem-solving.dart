c29626c4e7f1fc962c156e8020b5e6b52889cd85
jmawya <jmawya07@gmail.com>
2026-08-27 20:43:35 +0600

    Add largest number problem

--- bin/DSA-problem-solving.dart
+++ bin/DSA-problem-solving.dart
@@ -11,6 +11,18 @@ void main(){
   else{
     print('Zero');
   }
+  ///Given three integers, find the largest number without using any built-in function.
+  List<int> aa=[1,8,3,9];
+  int largest=aa[0];
+  for(int i=1;i<aa.length;i++){
+    if(aa[i]>largest){
+      largest=aa[i];
+      //print(largest);
+    }
+    //print(largest);
+
+  }
+  print(largest);
 
 
 
